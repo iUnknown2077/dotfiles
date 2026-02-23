893e920e4acc98e89601d538245a8e76fa92965e
iUnknown2077 <other.stuff.disarm786@passmail.net>
2026-02-23 14:39:50 +0100

    first commit

---
+++ end4-overrides/fish/overrides.fish
@@ -0,0 +1,25 @@
+# Export attribute for variables
+export MANPAGER="nvim +Man!"
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow'
+export EDITOR="nvim"
+
+# List directory contents
+alias ls='eza -l --icons'
+alias la='eza -a --icons'
+alias ll='eza -la --icons'
+
+# Change the working directory
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias grep="rg --color=auto"
+
+# System information tool
+alias ff="fastfetch"
+
+# The stupid content tracker
+alias ga="git add ."
+alias gcl="git clone"
+alias gcm="git commit -m"
+alias gpl="git pull"
+alias gps="git push"
