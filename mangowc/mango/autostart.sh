d8c4e6218b153cde2472613a2572dd53349baa15
iUnknown2077 <other.stuff.dissarm786@passmail.net>
2026-02-23 20:21:32 +0100

    mako

--- mangowc/mango/autostart.sh
+++ mangowc/mango/autostart.sh
@@ -2,6 +2,11 @@
 
 set +e
 
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1
+/usr/lib/xdg-desktop-portal-wlr >/dev/null 2>&1 &
+mako >/dev/null 2>&1 &
 swaybg -i ~/.config/mango/wallpapers/wall-1.png >/dev/null 2>&1 &
+waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
 /usr/lib/xfce-polkit/xfce-polkit >/dev/null 2>&1 &
