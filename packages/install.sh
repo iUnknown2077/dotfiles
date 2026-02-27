8475ef0de69e64f696e470b8253bdf1c3915fa1c
iUnknown2077 <other.stuff.disarm786@passmail.net>
2026-02-27 18:35:05 +0100

    improved install.sh

--- packages/install.sh
+++ packages/install.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 if ! command -v pacman >/dev/null 2>&1; then
   printf "pacman not found. It seems the system is not Arch Linux or an Arch-based distro. Aborting..."
   exit 1
 fi
 
 if ! command -v paru >/dev/null 2>&1; then
+
+  if ! command -v git >/dev/null 2>&1; then
+    sudo pacman -S --needed git
+  fi
+
   sudo pacman -S --needed base-devel rustup
   rustup default stable
-  git clone https://aur.archlinux.org/paru.git /tmp/build-paru
-  cd /tmp/build-paru
-  makepkg -si
-  rm -rf /tmp/build-paru
+
+  BUILD_DIR=$(mktemp -d /tmp/build-paru.XXXXXX)
+  git clone https://aur.archlinux.org/paru.git "$BUILD_DIR"
+  cd "$BUILD_DIR"
+  makepkg -si --noconfirm
+  cd -
+  rm -rf $BUILD_DIR
 
   if ! command -v paru >/dev/null 2>&1; then
-	  echo "Failed to install paru. Aborting..."
-	  exit 1
+    echo "Failed to install paru. Aborting..."
+    exit 1
   fi
 fi
 
 if [ -f "packages.txt" ]; then
-	paru -S --needed $(cat packages.txt | grep -v '^$')
-else 
-	echo "packages.txt not found. Skipping package installation."
+  paru -S --needed $(grep -v '^\s*$\|^\s*\#' packages.txt)
+else
+  echo "packages.txt not found. Skipping package installation."
 fi
 
 xdg-user-dirs-update
