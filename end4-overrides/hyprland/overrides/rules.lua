e26f0f016d2ed048348abbb245c5f3c7e59b6b12
iUnknown2077 <other.stuff.disarm786@passmail.net>
2026-08-19 16:00:14 +0200

    hyprland improvement

--- end4-overrides/hyprland/overrides/rules.lua
+++ end4-overrides/hyprland/overrides/rules.lua
@@ -1,3 +1,7 @@
+-- Transparency and blur
+--hl.window_rule({ match = { class = ".*" }, opacity = "0.89 override 0.89 override" })
+--hl.window_rule({ match = { class = ".*" }, no_blur = false })
+
 -- Apps
 hl.window_rule({ match = { class = "proton.vpn.app.gtk" }, workspace = "special silent" })
 hl.window_rule({ match = { class = "^([Ss]ignal)$" }, workspace = "4 silent" })
