8263a9065f5b34528bda43f726dea70832819591
iUnknown2077 <other.stuff.disarm786@passmail.net>
2026-08-30 15:22:23 +0200

    autostart fix

--- end4-overrides/hyprland/overrides/execs.lua
+++ end4-overrides/hyprland/overrides/execs.lua
@@ -1,8 +1,8 @@
 hl.on("hyprland.start", function()
 	-- Daemons
-	hl.exec_cmd("sleep 2 && protonvpn-app")
-	hl.exec_cmd("sleep 2 && pika-backup-monitor")
-	hl.exec_cmd("sleep 4 && cachy-update --tray")
+	hl.exec_cmd("sleep 3 && protonvpn-app")
+	hl.exec_cmd("sleep 3 && pika-backup-monitor")
+	hl.exec_cmd("sleep 3 && cachy-update --tray")
 
 	-- Apps
 end)
